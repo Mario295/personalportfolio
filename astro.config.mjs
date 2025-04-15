ab49265dd40e71dac55a549b089855fa6d3db72c
Mario295 <ogboriavwienefe@gmail.com>
2025-04-15 02:06:11 +0100

    Set up Github Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,12 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
+// Update with my github username and repo name
+const site = 'https://Mario295.github.io';
+const base = '/personalportfolio/';
+
 export default defineConfig({
+	site,
+	base,
 	integrations: [tailwind()],
 });
